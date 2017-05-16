199f731e4ff9d2874b99907c07a448e53f998b06
induction.lab <induction.lab@gmail.com>
2017-05-16 22:05:04 +0600

    -

--- _pull.cmd
+++ _pull.cmd
@@ -2,6 +2,6 @@
 cd "%~dp0"
 set GIT_HOME=d:\PROG\GIT\bin\
 set PATH=%PATH%;d:\PROG\GIT\bin\
-git clone https://github.com/induction-lab/ansible.git
-::git pull https://github.com/induction-lab/ansible.git
+::git clone https://github.com/induction-lab/ansible.git
+git pull https://github.com/induction-lab/ansible.git
 pause
--- _push.cmd
+++ _push.cmd
@@ -4,5 +4,5 @@ set GIT_HOME=d:\PROG\GIT\bin\
 set PATH=%PATH%;d:\PROG\GIT\bin\
 git add .
 git commit -m "-"
-git push https://github.com/induction-lab/absible.git
+git push https://github.com/induction-lab/ansible.git
 pause
