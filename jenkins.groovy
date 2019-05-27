82237693b49d36660afed1269601113d01455047
induction.lab <induction.lab@gmail.com>
2019-05-27 20:54:18 +0600

    -

--- jenkins.groovy
+++ jenkins.groovy
@@ -1,5 +1,7 @@
 node() {
   stage("x") {
+    def repoPath = pwd();
+    library identifier: 'local-lib@master', retriever: modernSCM([$class: 'GitSCMSource', remote: repoPath]);
     echo "ok"
   }
 }
