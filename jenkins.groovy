f9c9f8849fca53eb9be7ac2918e09d57cdcbbcbf
induction.lab <induction.lab@gmail.com>
2019-05-27 21:41:37 +0600

    -

--- jenkins.groovy
+++ jenkins.groovy
@@ -1,6 +1,12 @@
 node() {
   stage("x") {
-    def pipeline = library(identifier: 'local-lib@master', retriever: legacySCM(scm)).com.company.pipeline;
+    def pipeline = libraryFromLocalRepo().com.company.pipeline;
     def s = pipeline.Sample(5, 10);
   }
+}
+
+def libraryFromLocalRepo() {
+    // Workaround for loading the current repo as shared build lib.
+    // Checks out to workspace local folder named like the identifier.
+    library(identifier: 'local-lib@master', retriever: legacySCM(scm))
 }
