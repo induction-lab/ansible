0f4347ba2abce10a11841a7451b2e9f33c7ff4a2
induction.lab <induction.lab@gmail.com>
2019-05-28 23:21:47 +0600

    -

--- src/com/sbrf/pipeline/Sample.groovy
+++ src/com/sbrf/pipeline/Sample.groovy
@@ -1,19 +1,38 @@
 package com.sbrf.pipeline;
 
 import org.apache.commons.io.FilenameUtils;
-import com.lesfurets.jenkins.unit.BasePipelineTest;
 
-public class Sample extends BasePipelineTest {
-    private int x;
-    private int y;
+import org.junit.Before
+import org.junit.Test
 
-    public Sample(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
+import com.lesfurets.jenkins.unit.BasePipelineTest
+import static com.lesfurets.jenkins.unit.global.lib.LibraryConfiguration.library
+import static com.lesfurets.jenkins.unit.global.lib.LocalSource.localSource
+
+class Sample extends BasePipelineTest {
 
     @Override
-    public String toString() {
-        return "Sample{" + "x=" + x + ", y=" + y + "}";
+    @Before
+    void setup() {
+        String repositoryDirectoryName = FilenameUtils.getName(System.getProperty("user.dir"))
+        String dirPath = new File( System.getProperty("user.dir") )
+                .getAbsoluteFile()
+                .getParentFile()
+                .getAbsolutePath()
+
+        // This next call bypasses registerSharedLibrary; to allow registering a library with a different directory name
+        helper.libraries.put('my-jenkins-library', library(repositoryDirectoryName)
+                        .allowOverride(true)
+                        .implicit(false)
+                        .targetPath(dirPath)
+                        .retriever(localSource(dirPath))
+                        .build())
+    }
+
+    @Test
+    void should_run_example_pipeline_without_errors() throws Exception {
+        // def script = loadScript("test\\resources\\pipelines\\example1.pipeline")
+        // script.execute()
+        // printCallStack()
     }
 }
