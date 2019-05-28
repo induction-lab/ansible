092e7718299f6c262d1ead5ae4fdd0ee81d06799
induction.lab <induction.lab@gmail.com>
2019-05-28 22:47:12 +0600

    -

--- jenkins.groovy
+++ jenkins.groovy
@@ -3,6 +3,9 @@ node() {
     def pipeline = library(identifier: 'local-lib@master', retriever: legacySCM(scm)).com.sbrf.pipeline;
     def s = pipeline.Sample.new(5, 10);
     echo s.toString();
+    
+    setup();
+    
   }
 }
 
